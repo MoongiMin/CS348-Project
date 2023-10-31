f278c17579cb2e832be416bbcd6abfb1ea9e35ca
MoongiMin <102634988+MoongiMin@users.noreply.github.com>
2023-10-30 21:51:36 -0400

    Add files via upload

--- CS348 Project/main.py
+++ CS348 Project/main.py
@@ -1,8 +1,12 @@
 # main.py
 
 from flask import Flask, render_template, request
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, url_for
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 
 @app.route('/')
 def home():
@@ -16,6 +20,54 @@ def restaurant():
 
     # Render the restaurant page which could include a form to add a new restaurant
     return render_template('restaurant.html')
+
+
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            new_user = User(username=username)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+            return 'User created!'
+        else:
+            return 'User already exists!'
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            return 'Invalid username or password'
+        # Redirect to the restaurant page after successful login
+        return redirect(url_for('restaurant'))
+    return render_template('login.html')
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
